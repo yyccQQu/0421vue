6db97e80b2a42d1ada76aeef48c8851f4a69aea4
yyccQQu <643929860@qq.com>
2018-06-13 22:23:12 +0800

    打包调整完成

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -8,7 +8,7 @@ const config = {
   entry: path.join(__dirname, '../src/index.js'),
   output: {
     filename: 'bundle.[hash:8].js',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, '../dist'),
     // publicPath: 'http://127.0.0.1:8000/public/'
   },
   module: {
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -100,10 +100,5 @@ if (isDev) {
 
 }
 
-config.resolve = {
-    alias: {
-        'model': path.join(__dirname, '../client/model/client-model.js')
-    }
-}
 
 module.exports = config
