6e58e63095cbd4aa1e7e4261af05717c026aeaa2
yyccQQu <643929860@qq.com>
2018-06-16 12:17:08 +0800

    数据绑定、计算属性

--- practice/index.js
+++ practice/index.js
@@ -1,7 +1,7 @@
 // import './instance' //vue基础
-import './lifecycle' //生命周期
-// import './data-binding'
-// import './computed'
+// import './lifecycle' //生命周期
+// import './data-binding' //数据绑定
+import './computed' //计算属性
 // import './directive'
 // import './component'
 
